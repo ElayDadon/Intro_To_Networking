d33fd13c37a96f1d626bac3d87cd26f7b7f00386
yossiHad <yossihadad101@gmail.com>
2024-04-28 12:17:17 +0300

    finishing client basic functionalities

--- client.py
+++ client.py
@@ -1,69 +1,137 @@
 import socket
-import sys
-import select
-
+import struct
+import threading
+from typing import Tuple
 
 class TriviaClient:
-    MAGIC_COOKIE = '\xab\xcd\xdc\xba'
-    OFFER_MSG_TYPE = 2
-    BROADCAST_PORT = 13117
+    # Constants defining the network protocol
+    MAGIC_COOKIE = b'\xab\xcd\xdc\xba'  # Magic cookie to identify valid packets
+    OFFER_MSG_TYPE = 2  # Type of message indicating an offer
+    UDP_PACKET_LEN = 39  # Length of UDP packets expected
+    BROADCAST_PORT = 13117  # Port for broadcasting game offers
 
-    def __init__(self):
-        self.server_port = None
+    # Constants for message handling
+    MSG_LEN_HEADER = 4  # Header length indicating the length of the following message
+    TIME_TO_SEND_ANS = 10  # Time allowed for sending an answer
+    END_GAME_MSG = "Game over!"  # Message prefix indicating the end of the game
 
-    def wait_for_offer(self, udp_socket):
+
+    # Function to wait for a game offer from the server
+    def wait_for_offer(self, udp_socket: socket.socket) -> Tuple[str, str, int]:
         # Bind the UDP socket to a specific address and port
         udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         udp_socket.bind(("", self.BROADCAST_PORT))
-        print("Client started, listening for offer requests...")
-        data, server_addr = udp_socket.recvfrom(1024)
+        # Wait for the offer to the game and unpack it when received
+        data, server_addr = udp_socket.recvfrom(self.UDP_PACKET_LEN)
+        try:
+            (magic_cookie, msg_type, server_name, server_port) = struct.unpack('!4sB32sH', data)
+        except struct.error:
+            raise InvalidOffer("Invalid UDP packet structure")
+        # Check the magic cookie
+        if magic_cookie != self.MAGIC_COOKIE:
+            raise InvalidOffer("Invalid magic cookie")
+        # Check the message type
+        if msg_type != self.OFFER_MSG_TYPE:
+            raise InvalidOffer("Invalid message type (not offer)")
 
-    def start(self):
+        return server_name.decode().rstrip(), server_addr[0], server_port
+
+    # Function to connect to the game server over TCP
+    def connect_to_server(self, server_ip: str, server_port: int) -> socket.socket:
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect((server_ip, server_port))
+        return tcp_socket
+
+
+    # Function to handle the game
+    def play(self, tcp_socket: socket.socket, player_name: str) -> None:
+        # Send player name to the server
+        tcp_socket.sendall(f"{player_name}\n".encode())
+        # Print game rules
+        print("rules of the game:\n\tIf your answer is TRUE enter: Y / T / 1\n\tIf your answer is FALSE enter: N / F / 0")
+        while True:
+            # Receive message length header
+            msg_len = tcp_socket.recv(self.MSG_LEN_HEADER)
+            # Receive the message based on the received length
+            msg = tcp_socket.recv(int.from_bytes(msg_len, byteorder='big'))
+            print(msg)
+            # Check if the game is over
+            if msg[:len(self.END_GAME_MSG)] != self.END_GAME_MSG:
+                # If not, process player answer
+                self.process_player_answer(tcp_socket)
+            else:
+                break
+
+
+    # Function for getting and sending player's answer
+    def process_player_answer(self, tcp_socket):
+        # Event for stopping the answer sending thread
+        stop_flag = threading.Event()
+        # Thread to send the answer
+        send_key_t = threading.Thread(target=self.send_key, args=(tcp_socket, stop_flag))
+        send_key_t.start()
+        try:
+            # Wait for TIME_TO_SEND_ANS seconds for the player to enter an answer
+            send_key_t.join(timeout=self.TIME_TO_SEND_ANS)
+            stop_flag.set()
+        except threading.ThreadError:
+            print("Error while joining thread")
+
+    # Function for sending the player's answer
+    def send_key(self, tcp_socket: socket.socket, stop_flag: threading.Event) -> None:
+        # Get player input for the answer
+        key = input("Please enter your answer:")
+        # Validate the answer
+        while not self.is_valid_key(key) and not stop_flag.is_set():
+            print("Invalid answer")
+            key = input("Please enter your answer:")
+        if not stop_flag.is_set():
+            # If the stop flag is not set (the timeout didn't happen), send the answer to the server
+            tcp_socket.sendall(key.encode())
+
+    # Function to validate player answers
+    def is_valid_key(key: str) -> bool:
+        return key in ['N', 'F', '0', 'Y', 'T', '1']
+
+    # Function to start the trivia client loop
+    def start(self) -> None:
+        # Get player name
+        player_name = input("please enter your name")
+        print("Client started, listening for offer requests...")
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
             while True:
-                # wait for a game
                 try:
-                    self.wait_for_offer(udp_socket)
+                    # Wait for a game offer
+                    server_name, server_ip, server_tcp_port = self.wait_for_offer(udp_socket)
+                    print(f"Received offer from server {server_name} at address {server_ip}, attempting to connect...")
+                    try:
+                        # Connect to the server
+                        tcp_socket = self.connect_to_server(server_ip, server_tcp_port)
+                    except (socket.error, socket.timeout) as e:
+                        print('Could not connect to server:', str(e))
+                        continue
+                    try:
+                        # Play the game
+                        with tcp_socket:
+                            self.play(tcp_socket, player_name)
+                            print("Server disconnected, listening for offer requests...")
+                    except (socket.error, socket.timeout) as e:
+                        print(f"Communication error with the server: {e}")
+
                 except socket.timeout:
                     print('No server found')
                     continue
-                self.server_port = 3
-                server_port = int(data.decode())
-                print(f'Found server at {server_addr[0]}:{server_port}')
-                state = 'connecting_to_server'
-
-                if state == 'connecting_to_server':
-                    # Connect to the server
-                    try:
-                        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        tcp_socket.connect((server_addr[0], server_port))
-                    except socket.error as e:
-                        print('Could not connect to server:', str(e))
-                        sys.exit()
-
-                    # Set state to 'game_mode'
-                    state = 'game_mode'
-
-                elif state == 'game_mode':
-                    # Game mode
-                    # Collect characters from the keyboard and send them over TCP
-                    # Collect data from the network and print it on screen
-                    # Use select() to wait for both keyboard input and incoming data
-                    readable, writable, exceptional = select.select([tcp_socket, sys.stdin], [], [tcp_socket])
-
-                    for sock in readable:
-                        if sock == tcp_socket:
-                            # Handle incoming data
-                            data = sock.recv(1024)
-                            if not data:
-                                print('Disconnected from server')
-                                sys.exit()
-                            else:
-                                print('Received:', data.decode())
-
-                        elif sock == sys.stdin:
-                            # Handle keyboard input
-                            message = sys.stdin.readline().strip()
-                            tcp_socket.sendall(message.encode())
+                except InvalidOffer as e:
+                    print(e.message)
+                    continue
 
 
+class InvalidOffer(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
+if __name__ == '__main__':
+    trivia_client = TriviaClient()
+    trivia_client.start()
--- ip.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-
-def get_my_ip():
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname)
